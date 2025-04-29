39a317a830e439dcde0b9ead8317beae7b7b1740
João Souza <joaosvieira04@gmail.com>
2025-04-29 15:20:00 -0300

    refactor: Addes the new components into the principal archives

--- src/pages/sections/Hero/Hero.tsx
+++ src/pages/sections/Hero/Hero.tsx
@@ -2,42 +2,50 @@ import { Button, Grid, styled, Typography } from "@mui/material"
 import Avatar from "../../../assets/images/my_image.jpeg"
 import DownloadIcon from '@mui/icons-material/Download';
 import EmailIcon from '@mui/icons-material/Email';
+import StyledButton from "../../../components/StyledButton/StyledButton";
 
 function Hero() {
 
     const StyledHero = styled("div")(({theme}) => ({
         backgroundColor: theme.palette.primary.main,
         height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center", 
     }))
     
-    const StyledImg = styled("img")(()=> ({
-        width: "30%",
+    const StyledImg = styled("img")(({theme})=> ({
+        width: "70%",
         borderRadius: "50%",
-        padding: "10px",
+        boxShadow: `0 0 10px ${theme.palette.secondary.main}`,
     }))
 
     return (
         <>
             <StyledHero>
                 <Grid container spacing={2} maxWidth={"lg"}>
-                    <Grid size={{xs: 12, md: 4}}>
-                        <StyledImg src={Avatar} alt="Hero" style={{ width: "100%", height: "auto" }} />
+                    <Grid size={{xs: 12, md: 5}} display="flex" justifyContent="center" alignItems="center">
+                        <StyledImg src={Avatar} alt="Hero"/>
                     </Grid>
-                    <Grid size={{xs: 12, md: 8}} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-                        <Typography color="secondary" variant="h1" textAlign={"center"}>João Pedro</Typography>
+                    <Grid size={{xs: 12, md: 7}} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+                        <Typography color="secondary" variant="h1" textAlign={"center"} pb={2}>João Pedro</Typography>
                         <Typography color="primary.contrastText"variant="h2"textAlign={"center"}>I'm a Web Developer</Typography>
-                        <Grid container display={"flex"} justifyContent="center" alignItems="center">
+                        <Grid container display={"flex"} justifyContent="center" pt={3} spacing={3}>
                             <Grid size={{xs: 12, md: 6}} display={"flex"} justifyContent="center">
-                                <Button color="secondary" variant="contained" style={{margin: "2%"}}>
+                                <StyledButton>
                                     <DownloadIcon/>
-                                    Download CV
-                                </Button>
+                                    <Typography>
+                                        Download CV
+                                    </Typography>
+                                </StyledButton>
                             </Grid>
                             <Grid size={{xs: 12, md: 6}} display={"flex"} justifyContent="center">
-                                <Button color="secondary" variant="contained" style={{margin: "2%"}}>
+                                <StyledButton>
                                     <EmailIcon />
-                                    Contact Me
-                                </Button> 
+                                    <Typography>
+                                        Contact Me
+                                    </Typography>
+                                </StyledButton>
                             </Grid>
                         </Grid>
                     </Grid>
