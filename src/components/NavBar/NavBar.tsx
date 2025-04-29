7670e0eb836a1b923e3715be1d57d8c76018176c
João Souza <joaosvieira04@gmail.com>
2025-04-29 15:18:28 -0300

    feat: Creates new components

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,37 @@
+import { AppBar, MenuItem, styled, Toolbar } from "@mui/material"
+
+const NavBar = () => {
+
+    const StyledToolbar = styled(Toolbar)(({theme}) => ({
+        display: "flex",
+        justifyContent: "space-evenly",
+        backgroundColor: theme.palette.primary.main,
+        padding: "0 20px",
+        height: "60px",
+        '& .MuiMenuItem-root': {
+            alignItems: "center",
+            color: theme.palette.primary.contrastText,
+            fontSize: "1.2rem",
+            fontWeight: "bold",
+            '&:hover': {
+                backgroundColor: theme.palette.secondary.main,
+                transition: "0.5s",
+                cursor: "pointer",
+            }
+        },
+    }))
+
+    return(
+        <>
+            <AppBar position="absolute">
+                <StyledToolbar>
+                    <MenuItem>About</MenuItem>
+                    <MenuItem>Skills</MenuItem>
+                    <MenuItem>Projects</MenuItem>
+                </StyledToolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default NavBar
---
+++ src/components/StyledButton/StyledButton.tsx
@@ -0,0 +1,34 @@
+import { styled } from "@mui/material";
+
+interface StyledButtonProps {
+    children: React.ReactNode;
+}
+
+const StyledButton: React.FC<StyledButtonProps> = ({children}) => {
+
+    const StyledButton = styled("button")(({theme}) => ({
+        backgroundColor: "transparent",
+        border: `2px solid ${theme.palette.primary.contrastText}`,
+        borderRadius: "10px",
+        padding: "5px 15px",
+        width: "100%",
+        color: theme.palette.primary.contrastText,
+        display: "inline-flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "10px",
+        '&:hover': {
+            backgroundColor: theme.palette.secondary.main,
+            transition: "0.5s",
+            cursor: "pointer",
+        }
+    }))
+    
+    return(
+        <>
+        <StyledButton>{children}</StyledButton>
+        </>
+    )
+}
+
+export default StyledButton;
