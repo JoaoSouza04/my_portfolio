ccd26b8f5ced4b88d6273d5143ca97758001889f
João Souza <joaosvieira04@gmail.com>
2025-05-04 14:52:44 -0300

    refactor: Addes the two new sections into the home file

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,8 @@
 import NavBar from "../../components/NavBar/NavBar"
 import About from "../sections/About/About"
 import Hero from "../sections/Hero/Hero"
+import Projects from "../sections/Projects/Projects"
+import Skills from "../sections/Skills/Skills"
 
 function Home() {
 
@@ -9,6 +11,8 @@ function Home() {
         <NavBar />
         <Hero />
         <About />
+        <Skills />
+        <Projects />
       </>
     )
   }
